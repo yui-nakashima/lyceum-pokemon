e6369b7b5aa794f8e58def210933f99c0e450796
Nakashima <132863183+yui-nakashima@users.noreply.github.com>
2024-02-13 10:19:10 +0900

    Update pokemon.js

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,20 +1,19 @@
-import { ProxyAgent } from "proxy-agent";
+import { ProxyAgent } from "proxy-agent"; // Proxy設定用
 
-const agent = new ProxyAgent();
+const agent = new ProxyAgent(); // Proxy設定用
 
 /** ポケモンの取得 */
 export const findPokemon = async (name) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-    agent,
+    agent, // Proxy設定
   });
   const pokemon = await response.json();
   return pokemon;
 };
 
+// 元のコード（Proxy設定なし）
 // export const findPokemon = async (name) => {
-//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-//     agent,
-//   });
+//   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
 //   const pokemon = await response.json();
 //   return pokemon;
 // };
